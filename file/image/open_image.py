41997cd4772128eff3e1ad4609b7d802a3072cbd
shivamagrawal2014 <cse.shivam.agrawal@gmail.com>
2019-01-22 14:30:36 +0530

    Changes made to OpenFile dictionary

--- file/image/open_image.py
+++ file/image/open_image.py
@@ -11,15 +11,19 @@ class OpenImage(OpenFile):
 
     @property
     def open_image(self):
-        return super(OpenImage, self)._open_files()
+        return self._open_files()
+
+    @property
+    def image_folder_files(self):
+        return self._extension_folder_files
 
     @property
     def image_folders(self):
-        return super(OpenImage, self)._extensions_to_folder
+        return self._extensions_to_folder
 
     @property
     def image_files(self):
-        return super(OpenImage, self)._extensions_to_files
+        return self._extensions_to_files
 
     def _parse_image(self, file_path):
         return file_path
@@ -27,8 +31,9 @@ class OpenImage(OpenFile):
 
 if __name__ == '__main__':
     from pprint import pprint
-    image = OpenImage('/home/shivam/Documents/', ['jpg'])
+    image = OpenImage('/home/shivam/Documents', ['jpg'])
     open_image = image.open_image
     image_files = image.image_files
     image_folders = image.image_folders
-    pprint(open_image)
+    image_folders_files = image.image_folder_files
+    pprint(image_folders_files)
