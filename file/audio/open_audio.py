08fc1940a3a1972514cc2a2042bd19e830f5870d
shivamagrawal2014 <cse.shivam.agrawal@gmail.com>
2019-02-12 12:26:35 +0530

    Changes made to record

--- file/audio/open_audio.py
+++ file/audio/open_audio.py
@@ -7,6 +7,7 @@ from file.audio.parse_audio import ParseAudio
 class OpenAudio(OpenFile):
 
     def __init__(self, folder: str, extensions: List[str]):
+        assert all((ext in AUDIO_TYPE_EXTENSION for ext in extensions))
         super(OpenAudio, self).__init__(folder=folder, extensions=extensions)
         self.__parse_audio = ParseAudio()
 
@@ -28,10 +29,8 @@ class OpenAudio(OpenFile):
 
     def _parse_audio(self, file_path):
         audio_content = self.__parse_audio(file_path)
-        if audio_content != file_path:
-            pcm, frm_rate = audio_content
-        else:
-            pcm, frm_rate = [[0]], 0
+        if audio_content != file_path: pcm, frm_rate = audio_content
+        else: pcm, frm_rate = [[0]], 0
         return self._protofy_audio(pcm, frm_rate)
 
     def _protofy_audio(self, pcm, frame_rate):
